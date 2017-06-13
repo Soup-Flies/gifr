80dad0b72ae9e92c8089cd05909332b1dca35ebe
Ben Charais <benjcharais@gmail.com>
2017-06-12 22:51:21 -0600

    Styling done, working on navbar functionality

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,49 +1,62 @@
+var searchTag = "";
+var tagType = "";
+var apiUrl = "";
+var limit = 6;
+var offset;
 
 var apiKey = "&rating&api_key=dc6zaTOxFJmzC";
 var parameters = "&limit=" + limit + "&offset=" + offset;
 var api = "http://api.giphy.com/v1/gifs/search?q=";
-var searchTag = "";
-var tagType = "";
-var apiUrl = "";
-var limit = 10;
-var offset = 10;
 
 function runApi() {
 	tagType = $("#input").val();
-		var currentApiUrl = api + tagType + apiKey;
-		for (var i = 0; i < 1; i++) {
+		var currentApiUrl = api + tagType + parameters + apiKey;
 			$.getJSON(currentApiUrl,function(json) {
 				console.log(json);
 				$.each(json.data, function(index, value) {
 					$.get(value.images.original.url);
-					var newGif = $("<img class='giffy col-xs-6 col-md-3'>");
+					var newDiv = $("<div class='aspectMaint col-xs-6 col-md-3'>")
+					var newGif = $("<img class='giffy'>");
 					$(newGif).attr("src", value.images.original_still.url);
 					$(newGif).attr("data-name", value.images.original.url);
-					$("#gifContainer").append(newGif);
-				})
-			})
-		}
-		offset += 10;
+					$(newDiv).append(newGif);
+					$("#gifContainer").append(newDiv);
+				});
+			});
+		console.log(currentApiUrl);
 		$("#input").val("");
-}
+};
 
 $(document).ready(function() {
 	$("#input").on("keyup", function(event) {
-		console.log(event.keyCode);
 		if (event.keyCode == 13) {
-		runApi();
-		}
-	})
+			runApi();
+		};
+	});
 
 	$(".query_btn").on("click", function() {
 		runApi();
-	})
+	});
 
 	$("#gifContainer").on("click", ".giffy", function() {
 		console.log(this);
 		var temp = $(this).attr("src");
 		$(this).attr("src", $(this).attr("data-name"));
 		$(this).attr("data-name", temp);
-	})
-})
+	});
+
+	// $(window).scroll(function () {
+	// if ($(window).scrollTop() >= 330 ) {
+	// 	setTimeout(function() {
+	// 		$(".navSearch").append($(".inputCont"));
+	// 		$(".navbar").attr("margin-top", "30px");
+	// 	}, 50)
+	// } else if ($(window).scrollTop() <= 320) {
+	// 	setTimeout(function() {
+	// 		$(".inputMain").append($(".inputCont"));
+	// 		$(".navbar").attr("margin-top", "-60px");
+	// 	}, 50);
+	// }
+	// });
+});
 
