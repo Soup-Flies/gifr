6fdc9819e7cdcbead69d837286f585abcbb1856e
Ben Charais <benjcharais@gmail.com>
2017-06-18 22:15:42 -0600

    Minimum Viable Product

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,62 +1,71 @@
-var searchTag = "";
-var tagType = "";
-var apiUrl = "";
 var limit = 6;
-var offset;
+var offset = 0;
+var lastSearch = "";
+var firstSearch = true;
 
 var apiKey = "&rating&api_key=dc6zaTOxFJmzC";
-var parameters = "&limit=" + limit + "&offset=" + offset;
 var api = "https://api.giphy.com/v1/gifs/search?q=";
 
-function runApi() {
-	tagType = $("#input").val();
-		var currentApiUrl = api + tagType + parameters + apiKey;
-			$.getJSON(currentApiUrl,function(json) {
-				console.log(json);
-				$.each(json.data, function(index, value) {
-					$.get(value.images.original.url);
-					var newDiv = $("<div class='aspectMaint col-xs-6 col-md-3'>")
-					var newGif = $("<img class='giffy'>");
-					$(newGif).attr("src", value.images.original_still.url);
-					$(newGif).attr("data-name", value.images.original.url);
-					$(newDiv).append(newGif);
-					$("#gifContainer").append(newDiv);
-				});
-			});
-		console.log(currentApiUrl);
-		$("#input").val("");
+//Display returned gifs from API and add rating information to them
+function addGifs(still, original, rating) {
+	var newDiv = $("<div class='aspectMaint col-xs-6 col-md-3'>")
+	var newGif = $("<img data-toggle='tooltip' data-placement='bottom' class='giffy'>");
+	$(newGif).attr("src", still);
+	$(newGif).attr("data-name", original);
+	$(newGif).attr("title", "Rating: " + rating.toUpperCase());
+	$(newDiv).append(newGif);
+	$("#gifContainer").append(newDiv);
 };
 
+//Call the Giphy api, and setup variables to more easily handle the objects returned
+function runApi() {
+	var tagType = $("#input").val();
+	var parameters = "&limit=" + limit + "&offset=" + offset;
+	var currentApiUrl = api + tagType + parameters + apiKey;
+	$.getJSON(currentApiUrl,function(json) {
+		$.each(json.data, function(index, value) {
+			var rating = value.rating;
+			var animImg = value.images.original.url;
+			var stillImg = value.images.original_still.url;
+			$.get(animImg);
+			addGifs(stillImg, animImg, rating);
+		});	
+	});
+};
+
+//Control the current offset being called for based on repeat searches of same category
+function searchTest() {
+	var currentSearch = $("#input").val();
+	if (currentSearch == lastSearch || !lastSearch) {
+		offset += limit;
+	} else {
+		offset = 0;
+	}
+	lastSearch = $("#input").val();
+	$("#input").val("");
+}
+
 $(document).ready(function() {
+
+	//Take enter as input confirm versus a click
 	$("#input").on("keyup", function(event) {
 		if (event.keyCode == 13) {
 			runApi();
+			searchTest();
 		};
 	});
 
+	//Handling for the click of search button
 	$(".query_btn").on("click", function() {
 		runApi();
+		searchTest();
 	});
 
+	//Control still versus animation state for displayed Gifs
 	$("#gifContainer").on("click", ".giffy", function() {
-		console.log(this);
 		var temp = $(this).attr("src");
 		$(this).attr("src", $(this).attr("data-name"));
 		$(this).attr("data-name", temp);
 	});
-
-	// $(window).scroll(function () {
-	// if ($(window).scrollTop() >= 330 ) {
-	// 	setTimeout(function() {
-	// 		$(".navSearch").append($(".inputCont"));
-	// 		$(".navbar").attr("margin-top", "30px");
-	// 	}, 50)
-	// } else if ($(window).scrollTop() <= 320) {
-	// 	setTimeout(function() {
-	// 		$(".inputMain").append($(".inputCont"));
-	// 		$(".navbar").attr("margin-top", "-60px");
-	// 	}, 50);
-	// }
-	// });
 });
 
